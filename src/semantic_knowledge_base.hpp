cd5c6a164ebe5bab3074144f89a1bc21ddca93fe
Ken0182 <canstudy18@hotmail.com>
2025-10-21 03:57:14 -0700

    Update semantic_knowledge_base.hpp

--- src/semantic_knowledge_base.hpp
+++ src/semantic_knowledge_base.hpp
@@ -168,7 +168,16 @@ public:
      * @return true if successful
      */
     bool storeIDF(const std::string& tag, float idf, int docCount);
-    
+
+    **
+     * @brief Store configuration parameter
+     * @param key Parameter name
+     * @param value Parameter value
+     * @return true if successful
+     */
+    bool storeConfig(const std::string& key, float value);
+
+
     /**
      * @brief Compute IDF statistics from configuration corpus
      * @param allTags All tags from all configurations
